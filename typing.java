2f9dac80b7355e268e4c3fc952ef5abd585bc148
ouzayr-droid <salifououzerou25@gmail.com>
2025-11-21 23:21:44 +0100

    random trining

---
+++ typing.java
@@ -0,0 +1,13 @@
+public class typing {
+    byte b1 = 10, b2 = 20 ;
+    short p = 200 ;
+    int n = 500 ;
+    long q = 100 ;
+    float x = 2.5f ;
+    double y = 5.25 ;
+
+    // L'exercice consiste à trouver le type de chaque opération
+    // b1 + b2 = byte
+    // p + b1 = short + byte
+
+}
