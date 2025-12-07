25fbf3ec5f6ce2a5fcc706a4429bddd94d90c6f8
ouzayr-droid <salifououzerou25@gmail.com>
2025-12-07 18:24:37 +0100

    feature: CRUD LSC 1

--- listeSimplementChainees/ListeSimplementChainee.java
+++ listeSimplementChainees/ListeSimplementChainee.java
@@ -4,12 +4,41 @@ class Cellule
 {
     int valeur;
     Cellule suivant;
+
+    Cellule(int val, Cellule suiv)
+    {
+        this.valeur = val;
+        this.suivant = suiv;
+    }
 }
 
-class BaseDeListe
+/*  
+    * SimpleListe crée une méthode qui fait une initalisation de liste chainée
+    * Créer une cellule initial de base qui pointe vers null
+    * créer une méthode qui va permettre d'ajouter des valeurs à la liste
+    * créer une méthode pour savoir combien d'élément est-ce que nous avons dans notre liste
+*/
+
+class SimpleListe
 {
-    
+    int idListe; // pour l'identifiant de la liste chainée
+    int nombreElement; // pour stocker le nombre d'élément de la liste
+
+    Cellule maCellule = new Cellule(0, null);
+
+    // méthode pour afficher une liste
+    void AfficheValues()
+    {}
+
+    // méthode pour créer une nouvelle cellule
+    void CreerCellule()
+    {}
+
+    // méthode pour supprimer une cellule de la chaine
+    void supprimerCellule()
+    {}
 }
+
 
 public class ListeSimplementChainee {
     
